bb1350fd2bdb9b8f40727fb4df8d639179d4a3b4
Tim-luo <luotianshuai@gmail.com>
2015-10-29 09:16:22 +0800

    change_day1_admin
    homework_day1_admin
    change_for_encoding

--- admin.py
+++ admin.py
@@ -1,63 +1,63 @@
 #!/usr/bin/env python 
-#-*- coding:utf-8 -*_-
+#-*- coding:utf-8 -*-
 #I have Readme ,if you have any question you can check it !
 #
 import getpass
 import os
 
-print """ ��ӭ��¼����¼���̿���̨
-����1�������û�
-����2������û�
-����3��ɾ���û�
+print """欢迎登录，登录流程控制台
+输入1：解锁用户
+输入2：添加用户
+输入3：删除用户
 """
 
-function_name = raw_input("�����빦�ܣ�1-3����")
+function_name = raw_input("请输入功能（1-3）：")
 if function_name == "1":
-    print '''Ŀǰ�����˻�Ϊ��'''
+    print '''目前已锁账户为'''
     print "-------------------------------------------"
     f = file('lockuser','r')
     list_f = f.readlines()
-    for line_list in list_f:   #��ӡ�����б�start
+    for line_list in list_f:   #打印已锁列表start
         line_list = line_list.strip()
-        print line_list        #��ӡ�����б�end
+        print line_list        #打印已锁列表end
     f.close
     print "-------------------------------------------"
 
 
     
     for i in range(3):
-        name_1 = raw_input("�������û�����")
+        name_1 = raw_input("请输入用户名：")
         f = file('lockuser','r')   #
         new_f = file('new_lockuser','a') 
         list_file = f.readlines()
         for line in list_file:
             if name_1 in line:
-                line = line.replace(line,'\n')  #�滻������û�Ϊ�հ��У�
+                line = line.replace(line,'\n')  #替换输入的用户为空白行！
                 
-                print "�û�%s�ѽ����������µ�¼��" % name_1
-                continue   #ִ�гɹ�����������ѭ���������������ݲ�����
-            new_f.write(line)    #д���ļ�
+                print "用户%s已解锁，请重新登录！" % name_1
+                continue   #执行成功后跳出当次循环，后则后面的内容不保！
+            new_f.write(line)    #写入文件
             
         f.close()
         new_f.close()
-        os.rename('new_lockuser','lockuser')  #�滻���ļ�
+        os.rename('new_lockuser','lockuser')  #替换旧文件
         break
     else:
-        print "�û���Ч����������"
+        print "用户无效请重新输入"
 
 elif function_name == "2":
-    print '''Ŀǰ�����û�Ϊ��
-��ע���û���������'''
+    print '''目前已有用户为：
+请注意用户不能重名'''
     print "-------------------------------------------"
     f = file('userlist','r')   #
     new_f = file('new_userlist','a') 
-    list_file = f.readlines() #�������
+    list_file = f.readlines() #以行输出
     for line_list in list_file:   
         line_list = line_list.strip()
-        print line_list        #��ӡ�û��б�
+        print line_list        #打印用户列表
     print "--------------------------------------------"
-    add_username = raw_input("����������Ҫ��ӵ��û�����")
-    add_pwd = raw_input("������Ҫ����û������룺") 
+    add_username = raw_input("请输入如您要添加的用户名：")
+    add_pwd = raw_input("请输入要添加用户的密码：") 
     name_none = []
     name_none.append(add_username)
     name_none.append(add_pwd)
@@ -71,34 +71,34 @@ elif function_name == "2":
         open_userlist.close
         print "�û�%s�����"  % add_username             
 elif function_name == "3":
-    print '''Ŀǰ��Ч�û�Ϊ��'''
+    print '''目前有效用户为：'''
     print "-------------------------------------------"
     f = file('userlist','r')   #
     new_f = file('new_userlist','a') 
-    list_file = f.readlines() #�������
+    list_file = f.readlines() #以行输出
     for line_list in list_file:   
         line_list = line_list.strip()
-        print line_list        #��ӡ�û��б�
+        print line_list        #打印用户列表
     f.close
     print "-------------------------------------------"
     for i in range(3):
-        name_userlist = raw_input("������Ҫɾ�����û�����")
+        name_userlist = raw_input("请输入要删除的用户名：")
         f = file('userlist','r')   #
         new_f = file('new_userlist','w') 
         list_file = f.readlines()
         for line in list_file:
             if name_userlist in line:
-                line = line.replace(line,'\n')  #�滻������û�Ϊ�հ��У�
+                line = line.replace(line,'\n')  #替换输入的用户为空白行！
                 
                 print "�û�%s��ɾ����" % name_userlist
-                continue   #ִ�гɹ�����������ѭ���������������ݲ�����
+                continue   #执行成功后跳出当次循环，后则后面的内容不保！
             new_f.write(line)    #д���ļ�
             
         f.close()
         new_f.close()
-        os.rename('new_userlist','userlist')  #�滻���ļ�
+        os.rename('new_userlist','userlist')  #替换旧文件
         break
     else:
-        print "�û���Ч����������"
+        print "用户无效请重新输入"
 else:
-    print "��Ч�Ĺ�����������1-3��"
+    print "无效的功能码请输入1-3！"
