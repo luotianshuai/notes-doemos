b9bc1074a9cfe1cd071ff89f5c4f74451a2a9b76
Tim-luo <luotianshuai@gmail.com>
2015-11-11 23:32:44 +0800

    update home_work

--- day3/day3_test.py
+++ day3/day3_test.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
-#info = raw_input('\033[33;1mplease input your backen\033[0m')
+#info = raw_input('\033[33;1mplease input your backen\033[0m')\
+'''
 info = 'www.oldboy.org'
+
 with open('haproxy.conf','r') as f:
-    for i in f.readlines():
-        i = i.strip()
-        if info in i and i[0:6] == 'backen':
-            print i
+    for index in range(len(f.readlines())):
+        line = f.next()
+        print line
+        '''
+# Open a file
+'''
+fo = open("haproxy.conf", "r")
+print "Name of the file: ", fo.name
+
+for index in range(int(len(fo.readline()))):
+   line = fo.next()
+   print "Line No %d - %s" % (index, line)
+   '''
+fo = open("haproxy.conf", "r")
+print int(len(fo.readlines()))
