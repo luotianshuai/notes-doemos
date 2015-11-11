69fc0fdb455fda2d486b1bdccd9bf93bead84504
Tim-luo <luotianshuai@gmail.com>
2015-11-11 16:29:39 +0800

    add_test.day3

--- day3/day3_test.py
+++ day3/day3_test.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
-
+#info = raw_input('\033[33;1mplease input your backen\033[0m')
+info = 'www.oldboy.org'
 with open('haproxy.conf','r') as f:
     for i in f.readlines():
         i = i.strip('\n')
         if info in i and i[0:6] == 'backen':
+            print i
