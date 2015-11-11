cfc900ee4761dd56280509667cb118913e922b18
Tim-luo <luotianshuai@gmail.com>
2015-11-11 18:26:58 +0800

    add_test.day3

--- day3/day3_test.py
+++ day3/day3_test.py
@@ -3,8 +3,7 @@
 #info = raw_input('\033[33;1mplease input your backen\033[0m')
 info = 'www.oldboy.org'
 with open('haproxy.conf','r') as f:
-    for k,v in enumerate(f.readlines()):
-        v = v.strip()
-        #print k,v
-        if info in v and v[0:6] == 'backen':
-            print next(v)
+    for i in f.readlines():
+        i = i.strip()
+        if info in i and i[0:6] == 'backen':
+            print i
