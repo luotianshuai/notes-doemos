61af3009c321fd4e5da2d6118154ecaca25bda46
Tim-luo <luotianshuai@gmail.com>
2015-11-11 18:06:49 +0800

    add_test.day3

--- day3/modify.py
+++ day3/modify.py
@@ -9,7 +9,7 @@ def get_info(info):
         for i in f.readlines():
             i = i.strip('\n')
             if info in i and i[0:6] == 'backen':
-                if next(i)
+                return i
 
 
         else:
