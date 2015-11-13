603242184012ed947e6f4b8f8d4ff8c1fd5ab5e6
Tim-luo <luotianshuai@gmail.com>
2015-11-13 12:07:28 +0800

    2015.11.12update

--- day3/day3_test.py
+++ day3/day3_test.py
@@ -3,8 +3,8 @@
 #info = raw_input('\033[33;1mplease input your backen\033[0m')\
 
 #info = 'buy.oldboy.org'
-#info = 'www.oldboy.org'
-info = 'test.shuaige.org'
+info = 'www.oldboy.org'
+#info = 'test.shuaige.org'
 tim = []
 with open('haproxy.conf','r') as f:  #打开文件
     li = []   #创建一个新列表，后续加入
@@ -19,15 +19,24 @@ with open('haproxy.conf','r') as f:  #打开文件
             break
     new_li = li[k-1:]
     #print new_li
-
-
-    '''
     for m,n in enumerate(new_li):
         if 'backend' in n :
             tim.append(m)
-            print tim
         if info in n :
-            a = m
-            print a
-    print new_li[a:tim[a]]
+            start_backend = m
+            print start_backend
+    print tim
+    start_index =  tim.index(start_backend)
+    print start_index
+    if start_backend == tim[-1]:  #判断是否是最后一个backend
+        print '\033[32;1m这是最后一行\033[0m'
+        print new_li[m-1:]
+    else:
+        print '\033[32;1m这不是最后一行\033[0m'
+        backend_info =  new_li[tim[start_index]:tim[start_index+1]]
+    for l in backend_info:
+        print l
+    '''
+    for s in range(len(tim)):
+        print s
 '''
--- day3/modify.py
+++ day3/modify.py
@@ -5,15 +5,36 @@ import time
 import json
 
 def get_info(info):
-    with open('haproxy.conf','r') as f:
-        for i in f.readlines():
-            i = i.strip('\n')
-            if info in i and i[0:6] == 'backen':
-                return i
 
-
+    with open('haproxy.conf','r') as f:  #打开文件
+        li = []   #创建一个新列表，后续加入
+        tim = []
+        for i in f.readlines(): #循环列表
+            i = i.strip('\n') #取消回车符
+            li.append(i) #把读取的文件加入列表中
+        for k,v in enumerate(li,1):#循环列表并指定下标
+            if v[0:7] == 'backend': #判断bakend所在行的下标
+                break
+        new_li = li[k-1:]   #取出backend的范围，并整合成一个列表
+        #print new_li
+        for m,n in enumerate(new_li):  #循环backend的列表
+            if 'backend' in n :
+                tim.append(m)  #把所有append的小标加入到列表中用来做判断
+            if info in n :
+                start_backend = m  #取出开始的backend！
+        #print tim
+        start_index =  tim.index(start_backend)
+        print start_index
+        if start_backend == tim[-1]:  #判断是否是最后一个backend
+            print '\033[32;1m这是最后一行\033[0m'
+            last_info = new_li[m-1:]
+            for p in last_info:
+                print p
         else:
-            return "\033[31;1m对不起，无法找到您输入的backen\033[0m"
+            print '\033[32;1m这不是最后一行\033[0m'
+            backend_info =  new_li[tim[start_index]:tim[start_index+1]] #取出正确的范围
+            for s in backend_info:
+                print s
 
 if __name__ == '__main__':
     print '''\033[34;1m\
