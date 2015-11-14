fc16b7437ed092b844b74b327171bf2b8a271735
Tim-luo <luotianshuai@gmail.com>
2015-11-14 14:28:00 +0800

    cao !

--- day3/day3_test.py
+++ day3/day3_test.py
@@ -1,42 +1,36 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
-import json
-import re
+del_infos = '{"backend": "bbb.oldboy.org","record":{"server": "100.1.7.90","weight": 20,"maxconn": 3000}}'
+def del_backend(del_infos):
+    del_info = json.loads(del_infos)
+    a = del_info['backend']
+    b = get_backend(a) #调用自己写的查看参数判断输入信息是否存在！不存在退出出提示，存在继续！
+    if 'sorry' in b:
+        return b
+    del_server = del_info['record']['server']
+    backend_title = del_info['backend']
+    li = []
+    backend_index = []
+    with open('haproxy.conf','r') as f1,open('haproxy.conf.del','w') as f2: #打开文件
+        for i in f1.readlines(): #循环列表
+            i = i.strip('\n') #取消回车符
+            li.append(i) #把读取的文件加入列表中
+        for k,v in enumerate(li):#循环列表并指定下标
+            if 'backend' in v and 'backend' == v[0:7]:
+                backend_index.append(k) #把backend的所在的下标加入到列表中
+        for i in backend_index:  #循环backend的下标
+            if backend_title in li[i] and i != backend_index[-1]: #如果添加的backend在原配置文件中存在，那么在找到这个backend下标下面的server中添加server信息
+                for u in li[i:backend_index[backend_index.index(i)+1]]:
+                    if del_server in u:
+                        print li[i:backend_index[backend_index.index(i)+1]].index(li[i:backend_index[backend_index.index(i)+1]])
 
-add_infos = '{"backend": "eee.oldboy.org","record":{"server": "100.1.7.10000000","weight": 20,"maxconn": 30}}'
-add_info = json.loads(add_infos)
-a = 'backend %s \n\t\tserver %s weight %s maxconn %s' % (add_info['backend'],\
-add_info['record']['server'],add_info['record']['weight'],add_info['record']['maxconn'])
-#上面的a是定义，如果没有用户输入的backend，就从字典模板中获取相关的值添加到配置文件中
-b = '\t\tserver %s weight %s maxconn %s' % (add_info['record']['server'],\
-add_info['record']['weight'],add_info['record']['maxconn'])
-#上面的b的定义，如果backend存在直接从字典模板中获取相关的值添加到backend下面的配置文件中
-backend_title = add_info['backend']
-backend_index = []
-li = []   #创建一个新列表，后续加入
-with open('haproxy.conf','r') as f1,open('haproxy.conf.add','w') as f2: #打开文件
-    for i in f1.readlines(): #循环列表
-        i = i.strip('\n') #取消回车符
-        li.append(i) #把读取的文件加入列表中
-    for k,v in enumerate(li):#循环列表并指定下标
-        if 'backend' in v and 'backend' == v[0:7]:
-            backend_index.append(k) #把backend的所在的下标加入到列表中
-    #print backend_index
-    #print backend_index[0]
+            if backend_title in li[i] and i == backend_index[-1]: #判断如果最后一行找到用户输入的backend就直接添加到备份文件
+                for w in li[i:]:
+                    f2.write(w)
+                    f2.write('\n')
+                f2.write(b)
+                print '\033[32;1m您添加的backend已存在并且在最后一行，添加server信息完成\033[0m'
 
-    write_start = li[0:backend_index[0]-1]  #取出backend开头之前的数据并写入文件！
-    for s in write_start:
-        f2.write(s)
-        f2.write('\n')
-    li_backend= li[backend_index[0]:]
-    for i in li_backend:
-        if backend_title not in i:
-            f2.write(i)
-            f2.write('\n')
-            print 'hello'
-    '''
-    for i in li[backend_index[0]:]:
-        if backend_title not in i:
-            f2.write(i)
-            f2.write('\n')
-'''
+
+if __name__ == '__main__':
+    print del_backend()
--- day3/modify.py
+++ day3/modify.py
@@ -22,7 +22,7 @@ def get_backend(check_info):
             elif check_info in li[i] and i == backend_index[-1]:  #如果是最后一个打印所有
                 return li[i:]  #打印行
         else:
-            return '\033[32;1m对比起无法找到您输入的\033[31;1m%s\033[0m\033[32;1m的backend信息\033[0m' % check_info
+            return '\033[32;1msorrt we cant find \033[31;1m%s\033[0m\033[32;1mbackendinfo\033[0m' % check_info
 
 def add_backend(add_infos):
     add_info = json.loads(add_infos)
@@ -44,37 +44,67 @@ def add_backend(add_infos):
         for s in write_start:
             f2.write(s)
             f2.write('\n')
-        li_backend = li[backend_index[0]:]
-        #print li_backend[-1]
-
+        li_backend = li[backend_index[0]:]#定位backend信息，并整合为一个列表。
         for i in li_backend:
-            if backend_title not in i:
+            if backend_title not in i: #判断如果backend信息不在此次循环中那么写入至文件
                 f2.write(i)
                 f2.write('\n')
                 #print i
-                if i  == li_backend[-1]:
+                if i  == li_backend[-1]: #判断如果是最后一行backend信息都没有用户输入的backend信息那么添加新的backend
                     f2.write('\n')
                     f2.write(a)
-                    return '\033[31;1m您添加的backend不存在，以为您创建新的backend\033[0m'
-            if backend_title in i:
+                    return '\033[31;1msoor you add backend we not have ，will create new backend\033[0m'
+            if backend_title in i: #判断如果有backend那么追加文件
                 f2.write(i)
                 f2.write('\n')
                 f2.write(b)
                 f2.write('\n')
-                for m in li_backend[li_backend.index(i)+1:]:
+                for m in li_backend[li_backend.index(i)+1:]: #追加剩下的信息至文件中
                     f2.write(m)
                     f2.write('\n')
-                return '\033[32;1m您添加的backend已存在，为您追加信息server信息\033[0m'
+                return '\033[32;1myou add backend has bin，will add server info\033[0m'
 
-def del_backend(del_info):
+def del_backend(del_infos):
+    del_info = json.loads(del_infos)
+    a = del_info['backend']
+    b = get_backend(a) #调用自己写的查看参数判断输入信息是否存在！不存在退出出提示，存在继续！
+    if 'sorry' in b:
+        return b
+    del_server = del_info['record']['server']
+    backend_title = del_info['backend']
+    li = []
+    backend_index = []
+    with open('haproxy.conf','r') as f1,open('haproxy.conf.del','w') as f2: #打开文件
+        for i in f1.readlines(): #循环列表
+            i = i.strip('\n') #取消回车符
+            li.append(i) #把读取的文件加入列表中
+        for k,v in enumerate(li):#循环列表并指定下标
+            if 'backend' in v and 'backend' == v[0:7]:
+                backend_index.append(k) #把backend的所在的下标加入到列表中
+        for i in backend_index:  #循环backend的下标
 
-
+            if backend_title in li[i] and i != backend_index[-1]: #如果添加的backend在原配置文件中存在，那么在找到这个backend下标下面的server中删除server信息！
+                #print i
+                for r,t in enumerate(li[i:backend_index[backend_index.index(i)+1]],i+1): #循环列表并定义下标名称为当前back的下标+1
+                    if del_server in t:
+                        del li[r-1]
+                        break
+            if backend_title in li[i] and i == backend_index[-1]: #如果添加的backend在原配置文件中存在，那么在找到这个backend下标下面的server中删除server信息！
+                #print i
+                for r,t in enumerate(li[i:backend_index[backend_index.index(i)+1]],i+1):
+                    if del_server in t:
+                        del li[r-1]
+                        break
+        for h in li:
+            f2.write(h)
+            f2.write('\n')
 
 if __name__ == '__main__':
     print '''\033[34;1m\
 输入1获取ha记录
 输入2增加ha记录
-输入3删除ha记录\033[0m'''
+输入3删除ha记录
+输入0将退出程序\033[0m'''
 
     while True:
         num = raw_input('\033[32;1m请输入序列号：\033[0m')
@@ -87,6 +117,8 @@ if __name__ == '__main__':
         if num == '3':
             read = raw_input('\033[033;1m请输入你要删除的backend：\033[0m')
             print del_backend(read)
+        if num == '0':
+            break
 
 
 #www.oldboy.org
