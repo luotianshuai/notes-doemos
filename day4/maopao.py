fdd86a365e8a59bd427bf12dc43bc9de20b2b66c
Tim-luo <luotianshuai@gmail.com>
2015-11-16 17:53:53 +0800

    2015.11.12update

--- day4/maopao.py
+++ day4/maopao.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env pytohon
 #-*- coding:utf-8 -*-
-'''
+
 li = [13,22,6,99,11]
 
 for m in range(4):
@@ -35,7 +35,7 @@ for m in range(1):
         li[m] = num2
         li[m+1] = temp
 print  li
-'''
+
 '''冒泡算法for循环'''
 '''
 #第一种方法
@@ -50,11 +50,13 @@ for n in range(1,len(li)):
         li[m] = num2
         li[m+1] = temp
 print li
+'''
 
+'''
 冒泡算法：
 列表中有5个元素两辆进行比较，然后用中间值进行循环替换！
 既然这样，既然这样我们还可以用一个循环把上面的循环进行在次循环，用表达式构造出内部循环！
 
-'''
 
 li = [13,22,6,99,11]
+'''
