88eccfff819550f2942ca640d556055b006fce15
Tim-luo <luotianshuai@gmail.com>
2015-11-16 18:05:04 +0800

    2015.11.16 jottings to homework

--- day4/modify.py
+++ day4/modify.py
@@ -1,1 +1,16 @@
-__author__ = 'Administrator'
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+import json
+import time
+
+
+
+
+
+if __name__ == '__main__':
+    while True:
+        print '''\033[34;1m\
+输入1获取ha记录
+输入2增加ha记录
+输入3删除ha记录
+输入0将退出程序\033[0m'''
