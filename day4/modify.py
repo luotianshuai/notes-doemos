9184513b4175b3b59b40f71e2568b09f6a0252a5
Tim-luo <luotianshuai@gmail.com>
2015-11-16 22:12:35 +0800

    2015.11.16 22：12 update homework！

--- day4/modify.py
+++ day4/modify.py
@@ -1,9 +1,71 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 import json
-import time
+
+'''
+定义查看函数：
+1、打开文件 循环文件
+2、定义一个标志位：backend_flag = False
+3、if line.strip() == ('backend %s') %backend:
+      backend_flag = True
+      continue
+   找到已backend开头，并且后面跟上用户输入：www.oldboy.org
+   然后把backend_flag 标志位设置为True，并且跳出此次循环！
+4、if backend_flag and line.strip() ：
+   fetch_list.append(line)
+   如果标志位backend_flag为真并且不为空是就把这一行追加到列表
+5、if backend_flag and line.strip().startswith('backend'):
+       break
+   如果标志位backend_flag 为真并且开头为backend跳出循环
+
+解释：
+   在循环文件中，定义了标志位为False时候，if backend_flag and line.strip().startswith('backend'): 不执行的！
+   if backend_flag and line.strip(): 也是不执行的！
+   只有当匹配到用户输入的backend匹配时，if line.strip() == ('backend %s') %backend: 现在backend_flag = True
+   if backend_flag and line.strip(): 标志位为真，并且不为空时将执行，并且追加到列表中。
+
+   当遇到下一个backend的时候，现在标志位（backend_flag现在是True）
+   if backend_flag and line.strip().startswith('backend'):  这个就匹配上，将会退出整个循环！
+       break
+
+'''
+def get_backend(backend):
+    fetch_list = []
+    with open('haproxy.conf') as f:
+        backend_flag = False
+        for line in f:
+            if line.strip() == "backend %s" % backend:
+                backend_flag = True
+                continue
+            if backend_flag and line.strip().startswith('backend'):
+                break
+            if backend_flag and line.strip():
+                fetch_list.append(line)
+    return fetch_list
 
 
+'''
+定义添加函数：
+1、判断backend是否存在
+   不存在直接添加backend 和 record 信息
+2、backend存在
+   2.1、backend存在记录不存在，添加记录
+   2.2、backend存在记录也存在，推出并提示记录存在
+
+'''
+def add_backend(backend):
+    backend_title = backend.get('backend')
+    current_title = 'backend %s' % backend_title
+    crrent_record = backend
+    check_backend = get_backend(backend)
+    if check_backend:
+        pass
+    else:
+        '''backend如果不存直接添加'''
+        with open('haproxy.conf','r') as old_ha,open('haproxy.conf.new','w') as new_ha:
+            for line in old_ha:
+                new_ha.write(line)
+            new_ha.write()
 
 
 
@@ -14,3 +76,27 @@ if __name__ == '__main__':
 输入2增加ha记录
 输入3删除ha记录
 输入0将退出程序\033[0m'''
+        num = raw_input('\033[33;1m请输入您需要的功能：\033[0m')
+        if num == '1':
+            print '''
+            查询功能测试：
+            存在：www.oldboy.org 不存在：none.oldboy.org'''
+            read = raw_input('\033[33;1m请输入您要查看的backend名称：\033[0m')
+            get_info = get_backend(read)
+            if get_info:
+                for i in get_info:
+                    print i
+            else:
+                print "\033[31;1m无法找到您输入的backend请检查：%s是否正确\033[0m" % read
+        if num == '2':
+            read = raw_input('\033[33;1m请输入您要添加的信息：\033[0m')
+            '''{"backend": "test.oldboy.org","record":{"server": "100.1.7.9","weight": 20,"maxconn": 30}}'''
+            read_new = json.loads(read)
+            add_backend(read_new)
+
+
+
+
+
+
+
