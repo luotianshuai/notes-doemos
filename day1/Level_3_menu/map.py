3a5c320f7d03ecd343a46dedd72cbf47c5e8df45
Tim-luo <luotianshuai@gmail.com>
2015-12-02 13:19:50 +0800

    复习2015.12.2

--- day1/Level_3_menu/map.py
+++ day1/Level_3_menu/map.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
-#
+
 
 city_map = {"河北省":{"石家庄市":['平山县','无极县','灵寿县'],
                     "衡水市":['饶阳县','安平县','武强县']
@@ -14,41 +14,45 @@ city_map = {"河北省":{"石家庄市":['平山县','无极县','灵寿县'],
 }
 
 
-
-for i in range(3):  #循环3次
-    print "---------------------地图---------------------------"
+for i in range(3):  #循环3次防止无限调用
+    print "\033[31;1m---------------------地图---------------------------\033[0m"
     sheng_list = city_map.keys()  #获取省的列表
     for sheng_listname in sheng_list: #循环省的列表
-        print sheng_listname #打印省的列表
-    print "----------------------------------------------------"
-    city_name = raw_input("请输如您要查看的省名: ")  #获取市的名称
-    jump_up_flag = False
-    if city_name in city_map:           #检查输入是否为正确的省名
-        sheng_name = city_map[city_name]  #使用输入的信息作为下标,（省名）
-        shi_nl = sheng_name.keys()
+        print "\033[31;1m%s\033[0m" % sheng_listname #打印省的列表
+    print "\033[34;1m----------------------------------------------------\033[0m"
+    province_name = raw_input("\033[34;1m请输如您要查看的省名:\033[0m ")  #获取市的名称
+    jump_up_flag = False  #定义标志位用来跳出整个循环
+    if province_name in city_map:           #检查输入是否为正确的省名
+        sheng_name = city_map[province_name]  #如果用户输入的信息存在，使用用户输入的信息作为字典的key,（省名）
+        shi_nl = sheng_name.keys() #获取省名下的value
         while True:
-            print"-----------------%s包含的市为--------------------" % city_name
-            for  shi_list in shi_nl:
-                print shi_list         #打印市列表
+            print"\033[34;1m-----------------%s包含的市为--------------------\033[0m" % province_name
+            for shi_list in shi_nl:
+                print "\033[34;1m%s\033[0m" % shi_list         #打印市列表
             print "----------------------------------------------------"
-            shi_pl = raw_input("请输入你要查看的市名：")
-            if shi_pl in shi_nl:
-                print "-----------------%s包含的县为--------------------" % shi_pl
-                xian_nl = city_map[city_name][shi_pl]  #总结上面的输入，设置县列表
-                for xian_list in xian_nl:
-                    print xian_list
-                print "-----------------------------------------------------"
-            if shi_pl not in shi_nl:
-                print "你输入的信息无效，请重新输入！"
+            shi_pl = raw_input("\033[33;1m请输入你要查看的市名：\033[0m")  #获取用户的输入
+            if shi_pl in shi_nl: #如果市名存在
+                print "\033[33;1m-----------------%s包含的县为--------------------\033[0m" % shi_pl
+                xian_nl = city_map[province_name][shi_pl]  #获取县列表
+                for xian_list in xian_nl: #循环打印县列表
+                    print "\033[33;1m%s\033[0m" % xian_list
+                print "\033[31;1m-----------------------------------------------------\033[0m"
+            else:
+                print "\033[31;1m你输入的市名\033[34;1m%s\033[0m\033[31;1m不存在请重新输入！\033[0m" % shi_pl
                 continue
-            jump_shi = raw_input("请问是否退出：1 退出、2 返回最上层,任意输入返回上一层：")
-            if jump_shi == "1":
+            jump_shi = raw_input("\033[32;1m请问是否退出：1 退出、2 返回最上层,任意输入返回上一层：\033[0m")
+            '''判断用户的输入：如果输入1则返回上级目录并且修改标志位的值为True，在上一层代码有判断标志位的代码
+            如果标志位为True执行break将跳出整个循环
+            如果输入2.直接break跳出while循环到最上级目录！
+            '''
+            if jump_shi == "1":   #获取用户输入，并修改标志位为True
                 jump_up_flag = True
                 break
             if jump_shi == "2":
                 break
-            print "您输入的信息有误请重新输入！"
-    if jump_up_flag:
+    if jump_up_flag: #判断如果标志位为True，这里将执行，然后执行下面break代码退出整个循环。
         break
+    else:
+        print "\033[31;1m您输入的省名\033[34;1m%s\033[0m\033[31;1m不存在请重新输入\033[0m" % province_name
 else:
-    print "信息无效请重新输入！3次错误之后程序将退出！"
+    print "\033[31;1m信息无效请重新输入！3次错误之后程序将退出！\033[0m"
--- tianshuai/level_3_menu.py
+++ tianshuai/level_3_menu.py
@@ -14,7 +14,6 @@ city_map = {"河北省":{"石家庄市":['平山县','无极县','灵寿县'],
 }
 
 
-
 for i in range(3):  #循环3次防止无限调用
     print "\033[31;1m---------------------地图---------------------------\033[0m"
     sheng_list = city_map.keys()  #获取省的列表
@@ -31,23 +30,27 @@ for i in range(3):  #循环3次防止无限调用
             for shi_list in shi_nl:
                 print "\033[34;1m%s\033[0m" % shi_list         #打印市列表
             print "----------------------------------------------------"
-            shi_pl = raw_input("\033[33;1m请输入你要查看的市名：\033[0m")
-            if shi_pl in shi_nl:
+            shi_pl = raw_input("\033[33;1m请输入你要查看的市名：\033[0m")  #获取用户的输入
+            if shi_pl in shi_nl: #如果市名存在
                 print "\033[33;1m-----------------%s包含的县为--------------------\033[0m" % shi_pl
-                xian_nl = city_map[province_name][shi_pl]  #
-                for xian_list in xian_nl:
+                xian_nl = city_map[province_name][shi_pl]  #获取县列表
+                for xian_list in xian_nl: #循环打印县列表
                     print "\033[33;1m%s\033[0m" % xian_list
                 print "\033[31;1m-----------------------------------------------------\033[0m"
             else:
                 print "\033[31;1m你输入的市名\033[34;1m%s\033[0m\033[31;1m不存在请重新输入！\033[0m" % shi_pl
                 continue
             jump_shi = raw_input("\033[32;1m请问是否退出：1 退出、2 返回最上层,任意输入返回上一层：\033[0m")
-            if jump_shi == "1":
+            '''判断用户的输入：如果输入1则返回上级目录并且修改标志位的值为True，在上一层代码有判断标志位的代码
+            如果标志位为True执行break将跳出整个循环
+            如果输入2.直接break跳出while循环到最上级目录！
+            '''
+            if jump_shi == "1":   #获取用户输入，并修改标志位为True
                 jump_up_flag = True
                 break
             if jump_shi == "2":
                 break
-    if jump_up_flag:
+    if jump_up_flag: #判断如果标志位为True，这里将执行，然后执行下面break代码退出整个循环。
         break
     else:
         print "\033[31;1m您输入的省名\033[34;1m%s\033[0m\033[31;1m不存在请重新输入\033[0m" % province_name
