07d39ad4913fc9d62015160a99afc0d9315cffdb
Tim-luo <luotianshuai@gmail.com>
2015-12-10 18:29:01 +0800

    update for 2015.12.10

--- day7/Game/run_game.py
+++ day7/Game/run_game.py
@@ -8,7 +8,7 @@ import random
 
 @login_model.wrapper
 def run_game(arg):
-    if os.path.exists(arg):
+    if os.path.exists(arg):  #判断角色是否有存档
         print "账号存在是否登录，如果要创建新的账号"
     else:
         print "\033[31;1m您好，您目前没有账号，请创建新的账号"
@@ -35,7 +35,7 @@ o【$▅▆▇◤
 攻击力：2000
 
 '''
-        user_input = raw_input("\033[32;1m请输入您选择的职业:\033[0m")
+        user_input = raw_input("\033[32;1m请输入您选择的职业:\033[0m") #获取用户的输入创建角色
         if user_input == '1':
             game_name = raw_input(("\033[32;1m请输入您的角色名字：\033[0m"))
             obj1 = person_model.Game_pmodel(game_name,'战士',500,1000)
@@ -44,16 +44,34 @@ o【$▅▆▇◤
             zhandou = raw_input("\033[32;1m请问是否进入副本：\033[0m")
             if zhandou == '1':
                 while True:
-                    a = random.randrange(1,3)
-                    if a == 1:
-                        obj2 = person_model.Game_pmodel('怪物','普通怪',300,700)
+                    monster = random.randrange(1,3)
+                    if monster == 1:
+                        b = random.randrange(300,1000)
+                        a = random.randrange(300,1000)
+                        obj2 = person_model.Game_pmodel('怪物','普通怪',b,a)
                         obj1.detail()
                         obj2.detail()
                         action = raw_input("\033[31;1m请问是否攻击yes/no其他为no:\033[0m")
                         if action == 'yes':
-                            obj1
+                            while True:
+                                print "\033[31;1m战斗开始\033[0m"
+                                print type(obj1.blood)
+                                obj1.blood = obj1.blood - obj2.attack
+                                obj2.blood = obj2.blood - obj1.attack
+                                if obj1.blood <= 0:
+                                    print "\33[31;1m您已经死亡，请重新来过！\033[0m"
+                                    return
+                                if obj2.blood <= 0:
+                                    print "\033[32;1m怪物已死亡,角色升级\033[0m"
+                                    obj1.attack + 10
+                                    obj1.blood +20
+                                obj1.detail()
+                                obj2.detail()
+                                skill = raw_input("\033[32;1m请问是否使用技能\033[0m")
+
                     else:
-                        print a
+                        pass
+
 
             else:
                 print "\033[32;1m欢迎下次继续游戏\033[0m"
