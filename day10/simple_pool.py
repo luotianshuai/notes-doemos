73c69b1fe6a1b7a84edd8359e936ef257f743c57
luotianshuai <luotianshuai@gmail.com>
2016-01-13 15:17:50 +0800

    update for 2016.1.13

--- day10/simple_pool.py
+++ day10/simple_pool.py
@@ -38,3 +38,22 @@ for i in xrange(300):
     thread = pool.get_thread() #线程池10个线程，每一次循环拿走一个！默认queue.get()，如果队列里没有数据就会等待。
     t = thread(target=func,args=(i,pool))
     t.start()
+
+
+'''
+self.queue.put(threading.Thread) 添加的是类不是对象，在内存中如果相同的类只占一份内存空间
+并且如果这里存储的是对象的话每次都的新增都得在内存中开辟一段内存空间
+
+还有如果是对象的话：下面的这个语句就不能这么调用了！
+for i in xrange(300):
+    thread = pool.get_thread()
+    t = thread(target=func,args=(i,pool))
+    t.start()
+    通过查看源码可以知道，在thread的构造函数中：self.__args = args  self.__target = target  都是私有字段那么调用就应该这么写
+
+for i in xrange(300):
+    ret = pool.get_thread()
+    ret._Thread__target = func
+    ret._Thread__args = (i,pool)
+    ret.start()
+'''
