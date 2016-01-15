3c0e0d6940c83d517ba968421d99a27ed85e1d4e
luotianshuai <luotianshuai@gmail.com>
2016-01-15 11:53:15 +0800

    update for 2016.1.15  update paramiko_command_key

--- day11/review_paramiko_SFTPClient_key.py
+++ day11/review_paramiko_SFTPClient_key.py
@@ -8,7 +8,7 @@ private_key = paramiko.RSAKey.from_private_key_file('/root/.ssh/id_rsa')
 transport = paramiko.Transport(('192.168.7.100',22))
 transport.connect(username='root',pkey=private_key) #调用transport进行连接
 
-sftp = paramiko.SFTPClient.from_transport(transport)#然后创建SFTPClient并基于transport连接，把他俩做个绑定
+sftp = paramiko.SFTPClient.from_transport(transport)#然后创建SFTPClient并基于transport连接，把他俩做个绑定!在去连接
 
 sftp.put('testsftpfile.zip','/tmp/sftpfile-luotianshuai.zip') #将sftpfile.zip上传到目标机器的/tmp/sftpfile-luotianshuai.zip
 sftp.get('/tmp/messages.log','shuaige.log') #下载目标服务器/tmp/messages.log 到本地的shuaige.log文件（程序执行目录中）
