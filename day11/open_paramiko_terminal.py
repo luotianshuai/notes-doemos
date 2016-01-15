babad0ae3671f56e12d2f89d19367c6c1552eea8
luotianshuai <luotianshuai@gmail.com>
2016-01-15 16:13:59 +0800

    update for 2016.1.15  update oipen terminal

--- day11/open_paramiko_terminal.py
+++ day11/open_paramiko_terminal.py
@@ -3,6 +3,9 @@
 __author__ = 'luo_t'
 import paramiko
 import os
+import sys
+import select
+import socket
 
 tran = paramiko.Transport(('192.168.7.100', 22,))
 tran.start_client()
@@ -18,12 +21,31 @@ chan = tran.open_session()# 打开一个通道
 chan.get_pty()# 获取一个终端
 chan.invoke_shell()# 激活器
 
-#########
+'''
 # 利用sys.stdin,肆意妄为执行操作
 # 用户在终端输入内容，并将内容发送至远程服务器
 # 远程服务器执行命令，并将结果返回
 # 用户终端显示内容
-#########
+'''
+while True:
+    # 监视用户输入和服务器返回数据
+    # sys.stdin 处理用户输入
+    # chan 是之前创建的通道，用于接收服务器返回信息
+    readable, writeable, error = select.select([chan, sys.stdin, ],[],[],1)
+    if chan in readable:
+        try:
+            x = chan.recv(1024)
+            #ssh连接后他发送接收数据也是通过socket来做的
+            if len(x) == 0:
+                print '\r\n*** EOF\r\n',
+                break
+            sys.stdout.write(x)
+            sys.stdout.flush()
+        except socket.timeout:
+            pass
+    if sys.stdin in readable:
+        inp = sys.stdin.readline()
+        chan.sendall(inp)
 
 chan.close()
 tran.close()
