03f69007f00e20037c991bf8f8207db3781619e0
luotianshuai <luotianshuai@gmail.com>
2016-01-21 15:01:43 +0800

    update home work 2016-01-21

--- day12/homeworkday12/server/config/settings.py
+++ day12/homeworkday12/server/config/settings.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 __author__ = 'luotianshuai'
+import redis
 
-RedisServer = '192.168.17.15'
+RedisServer = '127.0.0.1'
 RedisPort = 6379
 RedisSubChannel = 'fm101'
 RedisPubChannel = 'fm100'
+
+
+#
+# if __name__ == '__main__':
+#     r = redis.Redis(host='127.0.0.1',port=6379)
+#     r.set('name','shuaige')
+#     ret = r.get('name')
+#     print ret
--- day12/homeworkday12/server/core/main.py
+++ day12/homeworkday12/server/core/main.py
@@ -2,15 +2,22 @@
 #-*- coding:utf-8 -*-
 __author__ = 'luotianshuai'
 
-import redishelper
+import os
+import sys
+from redishelper import RedisHelper
 import serialize
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) #获取文件所在的顶级目录，方便加载其他的模块
+sys.path.append(BASE_DIR) #加载环境变量
+
 from config import hosts
 
-class MonitorServer(object):
+class MonitorServer(object): #创建主的类，调用连接Redis&调用serialize序列化，把监控模板上传至Redis
     def __init__(self):
-        self.r = redishelper.RedisHelper()
+        self.r = RedisHelper()
         self.save_configs()
     def start(self):
         pass
     def save_configs(self):
-        serialize.push_config_toredis(hosts.monitored_groups)
+        serialize.push_config_toredis(self,hosts.monitored_groups)#这里把self传过去，在push_config_toredis中即可调用实例
