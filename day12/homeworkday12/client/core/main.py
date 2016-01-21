d62002e13eb9a4a6288f8ee397a302b24baa3e75
luotianshuai <luotianshuai@gmail.com>
2016-01-21 16:37:04 +0800

    update for 2016.1.19  update home work

--- day12/homeworkday12/client/core/main.py
+++ day12/homeworkday12/client/core/main.py
@@ -34,7 +34,8 @@ class MonitorClient(object):
                         self.host_config[servers].append(0)
                     plugin_name,interval,last_run_time = val
                     if time.time() - last_run_time < interval:#判断当前时间是否小于监控间隔
-                        pass
+                        next_run_time = interval - (time.time() - last_run_time )
+                        print "Service [%s] next run time is in [%s] secs" % (servers,next_run_time)
                     else:
                         print "------will to run the [%s] again------" % servers
                         last_run_time = time.time()
