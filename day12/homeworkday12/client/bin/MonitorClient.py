0da8386d17714cf7653eb7f3208c8dd148c07f4e
luotianshuai <luotianshuai@gmail.com>
2016-01-21 16:43:42 +0800

    update for 2016.1.19  update home work

--- day12/homeworkday12/client/bin/MonitorClient.py
+++ day12/homeworkday12/client/bin/MonitorClient.py
@@ -7,7 +7,7 @@ import sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) #获取文件所在的顶级目录，方便加载其他的模块
 sys.path.append(BASE_DIR) #加载环境变量
-
+print BASE_DIR
 from core import main
 from config import settings
 
