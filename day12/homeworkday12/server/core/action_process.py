215bb98ed44b76b6453644976e96457493637c59
Tim-luo <luotianshuai@gmail.com>
2016-01-23 04:36:52 +0800

    update homework check

--- day12/homeworkday12/server/core/action_process.py
+++ day12/homeworkday12/server/core/action_process.py
@@ -8,3 +8,7 @@ import serialize
 def action_process(server_instance,business_type,client_data):
     func = getattr(serialize,business_type)
     func(server_instance,client_data)
+
+def action_work(server_instance,keys_name):
+    func = getattr(serialize,keys_name)
+    func =
