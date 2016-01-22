4a0fd826d40075531bdc2a3f55d02abc77c9252f
Tim-luo <luotianshuai@gmail.com>
2016-01-23 04:47:05 +0800

    update homework check

--- day12/homeworkday12/server/core/serialize.py
+++ day12/homeworkday12/server/core/serialize.py
@@ -25,3 +25,7 @@ def report_monitor_data(main_server_instance,client_data):
 def linux_mem(main_instance,keys_name,hostip):
     data = main_instance.r.get(keys_name)
     print data
+
+def linux_cpu(main_instance,keys_name,hostip):
+    data = main_instance.r.get(keys_name)
+    print data
