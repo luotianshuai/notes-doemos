3903f1577fe41a1b3b9db0c30cb94aaaff25039c
luotianshuai <luotianshuai@gmail.com>
2016-01-22 16:01:11 +0800

    update for 2016.1.22  2:40update home work

--- day12/homeworkday12/server/core/serialize.py
+++ day12/homeworkday12/server/core/serialize.py
@@ -19,5 +19,5 @@ def report_monitor_data(main_server_instance,client_data):
     report_monitor_data.r.set('ServiceData::%s:%s' % (client_data['report_monitor_data']['ip_address'],
                                                client_data['report_monitor_data']['service_name']),
                                                client_data)
-    print "------------------------>>",main_server_instance.get((client_data['report_monitor_data']['ip_address'],
+    print "------------------------>>",main_server_instance.r.get((client_data['report_monitor_data']['ip_address'],
                                                client_data['report_monitor_data']['service_name']))
