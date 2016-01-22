2e1509ec09bbfbfe7c9c80c8786e14f78b3e18b0
Tim-luo <luotianshuai@gmail.com>
2016-01-23 06:32:53 +0800

    update homework check

--- day12/homeworkday12/server/core/serialize.py
+++ day12/homeworkday12/server/core/serialize.py
@@ -33,10 +33,10 @@ def report_monitor_data(main_server_instance,client_data):
                                                #client_data['report_monitor_data']['service_name']))
 
 def linux_mem(main_instance,keys_name): #内存监控判断
-    client_datas = main_instance.r.get(keys_name) #获取客户端返回的数据
+    client_datas = json.loads(main_instance.r.get(keys_name)) #获取客户端返回的数据
     print type(client_datas)
     print client_datas
-    json.loads(client_datas,encoding=unicode)
+
 
     mem = services.linux.Memory()#实例化Memory，取出监控值
     mem_parameter = mem.triggers.values()[0] #取出监控阀值信息
