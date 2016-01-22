7b8274942db93fd905f3290ff4fb22be3290bb24
Tim-luo <luotianshuai@gmail.com>
2016-01-23 04:19:53 +0800

    update homework check

--- day12/homeworkday12/server/core/main.py
+++ day12/homeworkday12/server/core/main.py
@@ -43,9 +43,11 @@ class MonitorServer(object): #创建主的类，调用连接Redis&调用serializ
 
     def monitor_data_handling(self):#所有的数据逻辑处理方法
         print '''---starting a new process to deal with monitor data ---'''
-        client_data = self.r.keys("ServiceData::*")
-        for i in client_data:
-            print i
+        while True:
+            client_data = self.r.keys("ServiceData::*")
+            for i in client_data:
+                print i
+                time.sleep(1)
 
     def save_configs(self):
         serialize.push_config_toredis(self,hosts.monitored_groups)#这里把self传过去，在push_config_toredis中即可调用实例
