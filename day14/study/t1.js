0554be8eb167b068553ba31e86674fd675f49ca4
luotim <luotianshuai@gmail.com>
2016-02-24 13:28:12 +0800

    update home worker!

--- day14/study/t1.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by luotim on 16/2/18.
- */
-
-function func() {
-    alert('Hello Shuaige')
-    //上面的一行代码需要注意,每一行代码结束后都要加上分号,目的是为后期进行压缩
-}
---
+++ tianshuai/chage_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+
+
+
+source = "罗天帅"
+
+for i in source:
+    num = ord(i)
+    print num
+    print bin(num).replace('b','')
