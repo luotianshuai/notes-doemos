6a0522cfee4b571e4815aa39af2eb401875ee1bf
Tim-luo <luotianshuai@gmail.com>
2016-03-05 09:13:12 +0800

    update_homework

--- day16/homework/js/ChouTi.js
+++ day16/homework/js/ChouTi.js
@@ -10,3 +10,55 @@ function Run_going(){
     var sub = content.substring(1,content.length);
     document.title = sub + firstChar;
     }
+
+//点赞操作
+
+$('.change_style1').hover(function () {
+    //滑进去的操作
+    $(this).children('div').css("background-position","0px -18px");
+},function () {
+    //划出后的操作
+    $(this).children('div').css("background-position","0px -38px");
+});
+
+//信息操作
+$('.change_style2').hover(function () {
+    //滑进去的操作
+    $(this).children('div').css("background-position","0px -78px");
+},function () {
+    //划出后的操作
+    $(this).children('div').css("background-position","0px -58px");
+});
+
+//收藏操作
+$('.change_style3').hover(function () {
+    //滑进去的操作
+    $(this).children('div').css("background-position","0px -138px");
+},function () {
+    //划出后的操作
+    $(this).children('div').css("background-position","0px -158px");
+});
+
+
+
+$(function(){
+        //当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
+        $(function () {
+            $(window).scroll(function(){
+                if ($(window).scrollTop()>100){
+                    $("#back-to-top").fadeIn(1500);
+                }
+                else
+                {
+                    $("#back-to-top").fadeOut(1500);
+                }
+            });
+
+            //当点击跳转链接后，回到页面顶部位置
+
+            $("#back-to-top").click(function(){
+                $('body,html').animate({scrollTop:0},1000);
+                return false;
+            });
+        });
+    });
