f05b020769a072cb000978a9515005cc8f825781
Tim-luo <luotianshuai@gmail.com>
2016-03-05 11:45:28 +0800

    home work done

--- day16/homework/js/ChouTi.js
+++ day16/homework/js/ChouTi.js
@@ -2,6 +2,28 @@
  * Created by luo_t on 3/4/2016.
  */
 
+//返回顶部操作
+$(function(){
+        //当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
+        $(function () {
+            $(window).scroll(function(){
+                if ($(window).scrollTop()>100){
+                    $("#back-to-top").fadeIn(1500);
+                }
+                else
+                {
+                    $("#back-to-top").fadeOut(1500);
+                }
+            });
+
+            //当点击跳转链接后，回到页面顶部位置
+
+            $("#back-to-top").click(function(){
+                $('body,html').animate({scrollTop:0},1000);
+                return false;
+            });
+        });
+    });
 
 //定义跑马灯实例
 function Run_going(){
@@ -41,24 +63,21 @@ $('.change_style3').hover(function () {
 
 
 
-$(function(){
-        //当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
-        $(function () {
-            $(window).scroll(function(){
-                if ($(window).scrollTop()>100){
-                    $("#back-to-top").fadeIn(1500);
-                }
-                else
-                {
-                    $("#back-to-top").fadeOut(1500);
-                }
-            });
+//热点操作
+$('#contrl1').hover(function () {
+   //滑进去把hot_m2隐藏
+    $('#hot_m1').removeClass('hide');
+    $('#hot_m2').addClass('hide')
 
-            //当点击跳转链接后，回到页面顶部位置
+}, function () {
+    
+});
 
-            $("#back-to-top").click(function(){
-                $('body,html').animate({scrollTop:0},1000);
-                return false;
-            });
-        });
-    });
+$('#contrl2').hover(function () {
+   //滑进去把hot_m2隐藏
+    $('#hot_m2').removeClass('hide');
+    $('#hot_m1').addClass('hide')
+
+}, function () {
+    
+});
