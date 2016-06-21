eae4e41454d793a03245d1c94730625f10165ac6
luotim <luotianshuai@gmail.com>
2016-06-21 12:06:32 +0800

    Cease to struggle and you cease to live.

---
+++ tianshuai/pool_test20160621.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+__author__ = 'luotianshuai'
+
+
+
+from multiprocessing import Pool
+class a(object):
+    def run(self):
+        print(1)
+x = a()
+pool = Pool(1)
+
+
+for i in range(0,4):
+    pool.apply_async(x.run,())
+pool.close()
+pool.join()
