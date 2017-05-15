cc8dee009645c52edffc30cf6c9db9da5ab0e328
luotianshuai <luotianshuai@sogou-inc.com>
2017-05-15 15:33:12 +0800

    update note python

--- Guide_27/fast_sort.py
+++ Guide_27/fast_sort.py
@@ -4,7 +4,8 @@
 import random
 import time
 
-def quick_sort(array,start,end):
+
+def quick_sort(array, start, end):
     if start >= end:
         return
     k = array[start]
@@ -16,8 +17,8 @@ def quick_sort(array,start,end):
         right_flag = end 默认为穿过来的列表总长度
         当left_flag 小与right_flag的时候成立,说明左右两边的小旗子还没有碰头(为相同的值)
         '''
-        #右边旗子
-        while left_flag < right_flag and array[right_flag] > k:#代表要继续往左一移动小旗子
+        # 右边旗子
+        while left_flag < right_flag and array[right_flag] > k:# 代表要继续往左一移动小旗子
             right_flag -= 1
         '''
         如果上面的循环停止说明找到右边比左边的值小的数了,需要进行替换
@@ -26,9 +27,9 @@ def quick_sort(array,start,end):
         array[left_flag] = array[right_flag]
         array[right_flag] = tmp
 
-        #左边旗子
+        # 左边旗子
         while left_flag < right_flag and array[left_flag] <= k:
-            #如果没有找到比当前的值大的,left_flag 就+=1
+            # 如果没有找到比当前的值大的,left_flag 就+=1
             left_flag += 1
         '''
         如果上面的循环停止说明找到当前段左边比右边大的值,进行替换
@@ -37,18 +38,18 @@ def quick_sort(array,start,end):
         array[left_flag] = array[right_flag]
         array[right_flag] = tmp
 
-    #进行递归把问题分半
-    quick_sort(array,start,left_flag-1)
-    quick_sort(array,left_flag+1,end)
+    # 进行递归把问题分半
+    quick_sort(array, start, left_flag-1)
+    quick_sort(array, left_flag+1, end)
 
 if __name__ == '__main__':
     array = []  # [69, 471, 106, 66, 149, 983, 160, 57, 792, 489, 764, 589, 909, 535, 972, 188, 866, 56, 243, 619]
     start_time = time.time()
     for i in range(50000):
         array.append(random.randrange(1000000))
-    quick_sort(array,0,len(array)-1)
+    quick_sort(array, 0, len(array)-1)
     end_time = time.time()
     print(array)
-    print(start_time,end_time)
+    print(start_time, end_time)
     cost_time = end_time - start_time
     print('Cost time is :%d' % cost_time)
