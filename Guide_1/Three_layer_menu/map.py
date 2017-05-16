09ea4a8ed77f2e6cb46bace4a650aef03854e19e
luotianshuai <luotianshuai@sogou-inc.com>
2017-05-16 14:45:06 +0800

    update note python guide README and flow chat

--- Guide_1/Three_layer_menu/map.py
+++ Guide_1/Three_layer_menu/map.py
@@ -1,51 +1,25 @@
 # !/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-city_map = {"河北省":{"石家庄市":['平山县','无极县','灵寿县'],"衡水市":['饶阳县','安平县','武强县'],},
-            "山东省":{"济南市":['济阳县','商河县','平阴县'],"青岛市":['市南区','市北区','市东区'],},
-            "河南省":{"洛阳市":['新安县','洛宁县','宜阳县'],"郑州市":['金水区','惠济区','上街区'],}}
+city_map = {"河北省": {"石家庄市": ['平山县', '无极县', '灵寿县'], "衡水市": ['饶阳县', '安平县', '武强县'], },
+            "山东省": {"济南市": ['济阳县', '商河县', '平阴县'], "青岛市": ['市南区', '市北区', '市东区'], },
+            "河南省": {"洛阳市": ['新安县', '洛宁县', '宜阳县'], "郑州市": ['金水区', '惠济区', '上街区'], }}
 
-# 设置一个标志位用来控制整个循环
-map_flag = False
-
-
-while not map_flag:
-    # 打印省份名称
-    for index,province in enumerate(list(city_map.keys())):
-        print(index,province)
-    # 选择省份
-    user_input_province = input("请输入您要选择的省份,或输入q退出程序:").strip()
-    if user_input_province.isdigit():
-        province_key = list(city_map.keys())[int(user_input_province)]
-        # 打印选择的省份的城市名称
-        while not map_flag:
-            for index,city in enumerate(list(city_map[province_key].keys())):
-                print(index,city)
-            # 选择城市
-            user_input_city = input("请输入你要选择的城市,输入b返回上级菜单或者输入q退出程序:").strip()
-            if user_input_city.isdigit():
-                while not map_flag:
-                    city_key = list(city_map[province_key].keys())[int(user_input_city)]
-                    for index, town in enumerate(city_map[province_key][city_key]):
-                        print(town)
-                    user_input_end = input("到达三层菜单结尾,请输入b返回上一层或者q退出程序:")
-                    if user_input_end == 'q':
-                        "再见如果想继续访问三级菜单请重新运行程序"
-                        break
-                    else:
-                        print("\033[31;1m请输入正确的序列号,或者输入q退出程序!\033[0m")
-            else:
-                if user_input_city == 'q':
-                    "再见如果想继续访问三级菜单请重新运行程序"
-                    break
-                else:
-                    print("\033[31;1m请输入正确的序列号,或者输入q退出程序!\033[0m")
-    else:
-        if user_input_province == 'q':
-            "再见如果想继续访问三级菜单请重新运行程序"
-            map_flag = True
-        else:
-            print("\033[31;1m请输入正确的序列号,或者输入q退出程序!\033[0m")
-
-
+last_layers = []  # 上一层
+current_layers = city_map  # 当前层
+while True:
+    if not current_layers:  # 如果当前层为空，输出提示
+        print("Empty layer.")
+    for i in current_layers:  # 打印当前层的key
+        print(i)
+    choice = input("Input your choice: ").strip()  # 输入想进入的地点
+    if choice in current_layers:  # 如果输入存在
+        last_layers.append(current_layers)  # 将当前层加入上一层列表
+        current_layers = current_layers[choice]  # 进入下一层
+    if choice == "q":  # 输入 q 退出程序
+        print("Exit.")
+        break
+    if choice == "b" and len(last_layers):  # 输入 b 并且上一级列表不为空，则取最后一条记录为当前层
+        current_layers = last_layers.pop()  # 取出上一次记录在列表中的最后一个元素
+        print("Super layer.")
 
