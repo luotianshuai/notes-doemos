c945421d37f76ed554d20f9bc80546992b39e188
luotianshuai <luotianshuai@gmail.com>
2017-12-29 18:46:08 +0800

    class-videw TemplateView get_context_data

--- x/app01/views.py
+++ x/app01/views.py
@@ -1,19 +1,30 @@
+import random
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 from django.shortcuts import HttpResponse
 # Create your views here.
 
 
-class MyView(View):
-    def get(self, request):
+class MyView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(MyView, self).get_context_data(**kwargs)
+        context['now'] = 'hello'
+        context['test'] = [random.randint(1, 100) for i in range(100)]
+        return context
+
+    def get(self, request, *args, **kwargs):
         # view logic
-        return HttpResponse('Hello World')
+        context = self.get_context_data()
+        return render(request, self.template_name, context)
 
 
 class GettingView(View):
     getting = 'Good Day'
 
-    def get(self):
+    def get(self, request):
         # view logic
         return HttpResponse(self.getting)
 
@@ -21,6 +32,6 @@ class GettingView(View):
 class MorningGreetingView(GettingView):
     getting = 'Morning Day'
 
-    def get(self):
+    def get(self, request):
         # view logic
         return HttpResponse(self.getting)
