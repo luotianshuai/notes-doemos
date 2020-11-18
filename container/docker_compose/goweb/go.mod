49f0867fd7837d07d5a834890ed0eae01b158641
shuai <luotianshuai@gmail.com>
2020-11-18 19:27:13 +0800

    更新docker compose  goweb

--- container/docker_compose/goweb/go.mod
+++ container/docker_compose/goweb/go.mod
@@ -3,8 +3,20 @@ module goweb
 go 1.15
 
 require (
-	github.com/go-redis/redis v6.15.9+incompatible
-	github.com/onsi/ginkgo v1.14.2 // indirect
-	github.com/onsi/gomega v1.10.3 // indirect
-	golang.org/x/tools/gopls v0.5.3 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/go-redis/redis/v8 v8.3.4
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/ugorji/go v1.2.0 // indirect
+	golang.org/x/crypto v0.0.0-20201116153603-4be66e5b6582 // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/sys v0.0.0-20201116194326-cc9327a14d48 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- container/docker_compose/goweb/main.go
+++ container/docker_compose/goweb/main.go
@@ -1,24 +1,53 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
 
-	"github.com/go-redis/redis"
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 )
 
+// 声明一个全局变量，这样别的地方也可是使用
+var redisClient *redis.Client
+
+func init() {
+	initRedisClient()
+}
+
 func main() {
-	redisdb := redis.NewClient(&redis.Options{
-		Addr:     "192.168.1.110:6379",
+	web := gin.Default()
+
+	web.GET("/", func(c *gin.Context) {
+		accessNum := incrValue()
+		megValue := fmt.Sprintf("access numbser is :%d", accessNum)
+		c.JSON(200, gin.H{"msg": megValue})
+	})
+
+	web.Run()
+
+}
+
+// 初始化Redis链接
+func initRedisClient() error {
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
 		Password: "",
 		DB:       0,
 	})
 
-	pong, err := redisdb.Ping().Result()
-	if err != nil {
-		fmt.Printf("can not connection redis.....")
-		panic(err)
-	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	fmt.Print(pong)
+	_, err := redisClient.Ping(ctx).Result()
+	return err
 
+}
+
+// 递增返回
+func incrValue() int64 {
+	ctx := context.Background()
+	hits := redisClient.Incr(ctx, "hits")
+	return hits.Val()
 }
